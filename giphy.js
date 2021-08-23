fc43882b99977fe056878e6fb807f8af79aeb960
Jackie Woodburn <Jackie.Woodburn@softwire.com>
2021-08-23 15:59:30 +0100

    Experimented with giphy api calls

---
+++ giphy.js
@@ -0,0 +1,22 @@
+const GetTrendingGifs = () => {
+    var container = document.getElementById('image-container');
+    const apiKey = `ZGEzOktO7mSe3rD9eWvg5kIO7Atksd17`;
+
+    // maybe implement await here?
+    fetch(`https://api.giphy.com/v1/gifs/trending?api_key=${apiKey}}&limit=10`)
+        .then(response => response.json())
+            .then(json => {
+                console.log(json);
+                console.log(json.data)
+                // json.data.map(gif => gif.images.fixed_height.url)
+                //     .forEach(url => container.appendChild(UrlToImg(url)))
+            })
+    return container;
+}
+
+const UrlToImg = (url) => {
+    var img = document.createElement('img');
+    img.src = url;
+    return img;
+}
+
